fe8baf4cfc7ff85dd733d540c56b8e6c831e23aa
chandruscm <chandru.guitar@gmail.com>
2018-03-03 14:36:30 +0530

    Add clue adapter

---
+++ app/src/main/java/com/semaphores/techathlon/adapters/ClueListAdapter.kt
@@ -0,0 +1,77 @@
+package com.semaphores.gofind.Adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.View
+import com.semaphores.gofind.Models.Clue
+import com.semaphores.techathlon.R
+import kotlinx.android.synthetic.main.clue.view.*
+import kotlinx.android.synthetic.main.clue_image.view.*
+import kotlinx.android.synthetic.main.clue_number.view.*
+
+class ClueListAdapter(val clues: MutableList<Clue>) : RecyclerView.Adapter<ClueListAdapter.ClueHolder>()
+{
+    val TAG = "ClueListAdapter"
+
+    override fun onBindViewHolder(holder: ClueHolder?, position: Int)
+    {
+        holder?.bind(clues[position])
+    }
+
+    override fun getItemCount(): Int
+    {
+        return clues.size
+    }
+
+    override fun getItemViewType(position: Int): Int
+    {
+        if (clues[position].isLocked)
+            return 0
+        else
+        {
+            if (clues[position].isImage)
+                return 2
+            else
+                return 1
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ClueHolder
+    {
+        when (viewType)
+        {
+            1 -> return ClueHolder(LayoutInflater.from(parent?.context).inflate(R.layout.clue, parent, false))
+            2 -> return ClueHolder(LayoutInflater.from(parent?.context).inflate(R.layout.clue_image, parent, false))
+            else -> return ClueHolder(LayoutInflater.from(parent?.context).inflate(R.layout.clue_locked, parent, false))
+        }
+    }
+
+    class ClueHolder(item: View) : RecyclerView.ViewHolder(item)
+    {
+        val TAG = "ClueHolder"
+
+        fun bind(clue: Clue)
+        {
+            with(clue)
+            {
+                when (itemViewType)
+                {
+                    1 ->
+                    {
+                        itemView.clue_number.text = (adapterPosition + 1).toString()
+                        itemView.clue_description.text = description
+                    }
+
+                    2 ->
+                    {
+                        itemView.clue_number.text = (adapterPosition + 1).toString()
+                        itemView.clue_image.clue_image.setImageResource(imageRes!!)
+                    }
+
+                    else -> return
+                }
+            }
+        }
+    }
+}
