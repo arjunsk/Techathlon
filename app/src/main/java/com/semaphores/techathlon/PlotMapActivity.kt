310f6f5dc985ca7deabe37fe27dd249a75af0ffa
chandruscm <chandru.guitar@gmail.com>
2018-03-03 11:28:09 +0530

    Initialize MapPlotActivity

---
+++ app/src/main/java/com/semaphores/techathlon/PlotMapActivity.kt
@@ -0,0 +1,13 @@
+package com.semaphores.techathlon
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PlotMapActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView()
+    }
+}
