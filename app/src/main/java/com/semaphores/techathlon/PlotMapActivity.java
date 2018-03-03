a993a2abc8a049a9d1f318a01f03864820ac3818
chandruscm <chandru.guitar@gmail.com>
2018-03-03 14:06:05 +0530

    Add Fab to BottomSheet

--- app/src/main/java/com/semaphores/techathlon/PlotMapActivity.java
+++ app/src/main/java/com/semaphores/techathlon/PlotMapActivity.java
@@ -2,14 +2,33 @@ package com.semaphores.techathlon;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 public class PlotMapActivity extends AppCompatActivity
 {
+    private BottomSheetBehavior bottomSheetBehavior;
+    private LinearLayout bottomSheetPlotMap;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plot_map);
+
+        initView();
+        initBottomSheet();
+    }
+
+    public void initView()
+    {
+        bottomSheetPlotMap = findViewById(R.id.bottom_sheet_plot_map);
+    }
+
+    public void initBottomSheet()
+    {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetPlotMap);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
     }
 }
